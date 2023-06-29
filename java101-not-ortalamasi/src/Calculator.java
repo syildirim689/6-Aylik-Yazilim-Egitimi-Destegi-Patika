91d4b8f2556bc368c8677fcb7a8a0dedcc236112
Serhat <syildirim689@gmail.com>
2023-06-29 15:51:16 +0300

    Hesap Makinesi

---
+++ java101-not-ortalamasi/src/Calculator.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Calculator {
+    public static void main(String[] args) {
+        int n1, n2, select;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("İlk sayıyı giriniz: ");
+        n1 = input.nextInt();
+        System.out.print("İkinci sayıyı giriniz: ");
+        n2 = input.nextInt();
+
+        System.out.println("1- Toplama\n2- Çıkarma\n3- Çarpma\n4- Bölme");
+        System.out.print("Seçiminiz: ");
+        select = input.nextInt();
+
+        switch (select) {
+            case 1 -> System.out.println("Toplam: " + (n1 + n2));
+            case 2 -> System.out.println("Çıkarma: " + (n1 - n2));
+            case 3 -> System.out.println("Çarpma: " + (n1 * n2));
+            case 4 -> System.out.println("Bölme: " + (n1 / n2));
+            default -> System.out.println("Yanlış seçim yaptınız.");
+        }
+    }
+}
