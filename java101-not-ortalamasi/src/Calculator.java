e12ff23bda10cfb609371738680105829e014c08
Serhat <syildirim689@gmail.com>
2023-06-29 15:53:11 +0300

    Update Calculator.java

--- java101-not-ortalamasi/src/Calculator.java
+++ java101-not-ortalamasi/src/Calculator.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
+        // değişkenler
         int n1, n2, select;
         Scanner input = new Scanner(System.in);
-
+        // kullanıcıdan veri alma
         System.out.print("İlk sayıyı giriniz: ");
         n1 = input.nextInt();
         System.out.print("İkinci sayıyı giriniz: ");
         n2 = input.nextInt();
-
         System.out.println("1- Toplama\n2- Çıkarma\n3- Çarpma\n4- Bölme");
         System.out.print("Seçiminiz: ");
         select = input.nextInt();
-
+        // işlem tercihine göre sonuç yazdırma
         switch (select) {
             case 1 -> System.out.println("Toplam: " + (n1 + n2));
             case 2 -> System.out.println("Çıkarma: " + (n1 - n2));
